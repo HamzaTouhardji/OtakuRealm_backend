aaf222b18e446b748e5732b127f871851a73441d
hamza <h.touhardji@gmail.com>
2021-04-11 21:31:00 +0200

    rien

--- recommandation/urls.py
+++ recommandation/urls.py
@@ -28,6 +28,9 @@ urlpatterns = [
 
     path('register/', RegisterAPI.as_view(), name='register'),
     path('login/', CustomAuthToken.as_view(), name='login'),
+    
+    
+    
     path('logout/', knox_views.LogoutView.as_view(), name='logout'),
     path('logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
 ]
