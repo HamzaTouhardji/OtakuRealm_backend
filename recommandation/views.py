e94ef8ce64a63421cca1f48935360ee075b501ca
hamza <h.touhardji@gmail.com>
2021-04-17 22:06:59 +0200

    test commit

--- recommandation/views.py
+++ recommandation/views.py
@@ -172,7 +172,7 @@ class UserGenreList(APIView):
     def post(self, request):
         data = request.data
         #self.request.user.id
-        curent_utilisateur = Utilisateur.objects.get(user = request.data.id)
+        curent_utilisateur = Utilisateur.objects.get(user = request.data['id'])
         for genre in data["genres"]:
             if not Preferer.objects.filter(id_genre=genre["id"], id_utilisateur=curent_utilisateur.id).exists():
                 new_prefere = Preferer.objects.create(
